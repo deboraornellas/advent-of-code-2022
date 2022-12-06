e816f91cd1bfd87587cfa68623b67ed6ceb46ffb
Débora Ornellas <debora.barreto.ornellas@lego.com>
2022-12-06 07:51:08 +0000

    light refactoring

--- src/day5/day5.part2.ts
+++ src/day5/day5.part2.ts
@@ -1,13 +1,11 @@
-import { parseCratesAsArrays, parseMoveCommands } from './day5';
+import { parseCratesAsArrays, parseMoveCommands } from '../utils/helpers';
 
 export const day5part2 = (input: string[]) => {
   let text = '';
   const crateQueues = parseCratesAsArrays(input[0]);
   const commands = parseMoveCommands(input[1]);
   commands.forEach((command) => {
-    const numberOfCratesToMove = Number(command[0]);
-    const fromCrate = Number(command[1]);
-    const toCrate = Number(command[2]);
+    const [numberOfCratesToMove, fromCrate, toCrate] = command;
     const toBeMoved = crateQueues[fromCrate - 1].splice(-numberOfCratesToMove);
     crateQueues[toCrate - 1] = crateQueues[toCrate - 1].concat(toBeMoved);
   });
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -17,3 +17,40 @@ export const findStartOfMessageMarker = (
   }
   return counter;
 };
+
+export const parseCratesAsArrays = (crateInput: string) => {
+  const split = crateInput.split('\n');
+  const lastRow = split.pop() || '';
+
+  const numberOfCrates = lastRow.trim().split('  ').length;
+
+  let matrix: string[][] = [[]];
+  for (let i = numberOfCrates - 1; i >= 0; i--) {
+    if (!split[i]) continue;
+    const line = split[i].replace(/\x20{4}/g, ' _').replace(/\x20{1}/g, '');
+    const splitLine = line.replace(/\[/g, '').replace(/\]/g, '').split('');
+    splitLine.forEach((crate, i) => {
+      if (crate !== '_') {
+        if (matrix[i]) {
+          matrix[i].push(crate);
+        } else matrix[i] = [crate];
+      }
+    });
+  }
+  return matrix;
+};
+
+export const parseMoveCommands = (commandInput: string) => {
+  const commands: number[][] = [];
+  commandInput.split('\n').forEach((command) => {
+    commands.push(
+      command
+        .replace('move ', '')
+        .replace('from ', '')
+        .replace('to ', '')
+        .split(' ')
+        .map((x) => Number(x)),
+    );
+  });
+  return commands;
+};
